6161e14080c78280d28063db16b9c98a9c85f6c9
Code-Artist <codearteng@gmail.com>
2021-05-14 10:42:10 +0800

    .NET 5.0 Support

---
+++ ExtensionsTests/ObjectExtensionsTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using System.Windows.Forms;
+
+namespace ExtensionsTests
+{
+    [TestFixture]
+    public class ObjectExtensionsTests
+    {
+        [Test]
+        public void CloneObject()
+        {
+            Button b1 = new Button() { Name = "BtTest", Width = 123, Height = 50, Padding = new Padding(5) };
+            Button b2 = b1.Copy();
+
+            b1.Padding = new Padding(0);
+            b1.Width = 10;
+            Assert.AreEqual(5, b2.Padding.All);
+            Assert.AreEqual(123, b2.Width);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # CodeArtEng.Extensions
-Collection of .NET extensions methods.<br/>
+
+![NuGet](https://img.shields.io/nuget/v/CodeArtEng.Extensions)<br>
+Collection of .NET extensions methods. Support .NET 5.0 Framework<br/>
 Visit http://codearteng.blogspot.my/p/net-libraries.html for more information.
